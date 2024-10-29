2c65e659331c33750ea87ee34e8d6b73d573f656
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2024-10-29 18:38:39 +0100

    initial commit

---
+++ src/main/java/com/eNotesBackend/ENotesRestApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eNotesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENotesRestApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENotesRestApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eNotesBackend/ENotesRestApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eNotesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesRestApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
